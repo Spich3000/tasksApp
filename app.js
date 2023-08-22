620950b6984bb66bd0751d35cfe12e0b3ae53941
Spich <spichakovda@gmail.com>
2023-08-23 00:44:44 +0300

    routes

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 const tasks = require("./routes/tasks");
 
 
+// middleware
+app.use(express.json());
+
+
 // Routes
 app.get("/", (req, res) => {
     res.send("Task Manager App")
