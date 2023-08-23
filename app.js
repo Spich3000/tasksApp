1bb6612f8daba04877fdbaae4058198554ee3a07
Spich <spichakovda@gmail.com>
2023-08-23 05:16:36 +0300

    setting up mongodb

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
-
+const connectDB = require("./db/connect")
+require("dotenv").config()
 
 // middleware
 app.use(express.json());
@@ -18,7 +19,15 @@ app.get("/", (req, res) => {
 app.use("/api/v1/tasks", tasks)
 
 
-
 const port = 5050
 
-app.listen(port, console.log(`Server is listening ${port}`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening ${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start() 
---
+++ db/connect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+   return mongoose
+    .connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+}
+
+module.exports = connectDB
+
+// .then(() => console.log("Connected to the DB"))
+// .catch((err) => console.log(err))
